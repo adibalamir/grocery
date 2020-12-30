041e4cc7e2207cbc6db867eec50f5bea4837be90
Adih <adibalamir@hotmail.com>
2020-12-30 13:52:09 -0500

    add useeffect and backend

--- frontend/public/src/App.js
+++ frontend/public/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function GroceryList(props) {
   return <ul className="grocery-list">
@@ -10,13 +10,26 @@ function GroceryList(props) {
 }
 
 function App() {
-  const [groceryList, addGroceryItem] = useState([])
+  const [groceryList, addGroceryItem] = useState([]);
   const [input, setInput] = useState("");
+  // const [testString, setTestString] = useState("Testing");
+
+  // function callAPI() {
+  //   fetch("http://localhost:9000/testAPI")
+  //       .then(res => console.log(res.text()))
+  //       .then(res => console.log(res));
+  // }
+
+  useEffect(() => {
+    console.log("useEffect called");
+    // callAPI();
+  });
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Shopping List</h1>
+        {/* <p>{testString}</p> */}
       </header>
 
       <input name="new-grocery-item" placeholder="Add a new item." value={input} onChange={(e) => setInput(e.target.value)} />
