97eebeee00e8f7f8fdf85a8abd4c3a5dd2861620
Adih <adibalamir@hotmail.com>
2021-01-03 20:20:59 -0500

    Clear input when submitting a new item.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 
 function GroceryList(props) {
-  return <ul className="grocery-list">
+  return <ul className="grocery-list row">
     {props.list.length > 0 ? props.list.map(item => {
       return <li className="grocery-item" key={item.length}>{item}</li>
     }) : <li className="grocery-item">You have no items</li>}
@@ -20,15 +20,23 @@ function App() {
       })
   })
 
+  function onSubmit(groceryList, input) {
+    addGroceryItem([...groceryList, input]);
+    setInput("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header row">
         <h1>Shopping List</h1>
       </header>
       
-      <input name="new-grocery-item" placeholder="Add a new item." value={input} onChange={(e) => setInput(e.target.value)} />
-      <button className="submit-grocery-item" onClick={() => addGroceryItem([...groceryList, input])}>Submit</button>
       <GroceryList list={groceryList} />
+      
+      <div className="row">
+        <input name="new-grocery-item" placeholder="Add a new item." value={input} onChange={(e) => setInput(e.target.value)} />
+        <button className="submit-grocery-item" onClick={() => onSubmit(groceryList, input)}>Submit</button>
+      </div>
     </div>
   );
 }
